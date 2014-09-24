681af05991b91107b4c444ba7ac3bf7243e86f4c
Bryan Bonilla <bapd_emc@hotmail.com>
2014-09-24 13:47:57 -0500

    first commit

---
+++ desktop/src/com/reactivesource/tg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.reactivesource.tg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.reactivesource.tg.TuneGraph;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TuneGraph(), config);
+	}
+}
---
+++ html/src/com/reactivesource/tg/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.reactivesource.tg.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.reactivesource.tg.TuneGraph;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TuneGraph();
+        }
+}
